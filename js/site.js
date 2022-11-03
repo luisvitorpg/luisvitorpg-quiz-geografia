b5c6352f076649dc5af053f3b92e00636e881285
luisvitorpg <99150383+luisvitorpg@users.noreply.github.com>
2022-11-03 10:38:30 -0300

    criacao da pagina 2

--- js/site.js
+++ js/site.js
@@ -1,7 +1,17 @@
 function true1() {
     alert("Acertou! Muito bem!");
+    window.location = 'pagina2.html';
 }
 
 function false1() {
-    alert("Errou! Não fique triste, tente denovo!")
+    alert("Errou! Não fique triste, tente de novo!");
+}
+
+function true2() {
+    alert("Acertou! Você é fera!");
+    window.location = 'pagina3.html';
+}
+
+function false2() {
+    alert("Errou! Na próxima você consegue!");
 }
